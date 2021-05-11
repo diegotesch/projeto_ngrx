58e5870a0917a519d73fffa5237479dd7211a5cb
Diego Tesch <diego.tesch@gmail.com>
2021-05-11 17:15:46 -0300

    primeiro effect

--- src/app/store/actions/customer-support.actions.ts
+++ src/app/store/actions/customer-support.actions.ts
@@ -5,3 +5,8 @@ export const sendingCustomerSupportMessage = createAction(
   '[Customer Support Component] Sending Customer Support Message',
   props<{data: CustomerMessage}>()
 );
+
+export const sendMessageStatus = createAction(
+  '[Customer Support Effect] Sending Customer Message Status',
+  props<{isSentSuccess: boolean}>()
+);
--- src/app/store/redurecrs/customer-suport.reducer.ts
+++ src/app/store/redurecrs/customer-suport.reducer.ts
@@ -1,4 +1,5 @@
-import { sendingCustomerSupportMessage } from './../actions/customer-support.actions';
+import * as fromSupportActions from './../actions/customer-support.actions';
+// import { sendingCustomerSupportMessage, sendMessageStatus } from './../actions/customer-support.actions';
 import { Action, createReducer, on } from '@ngrx/store';
 
 
@@ -6,20 +7,30 @@ export const customerSuportFeatureKey = 'customerSuport';
 
 export interface State {
   name: string;
+  isSentSuccess: boolean | null;
 }
 
 export const initialState: State = {
   name: null,
+  isSentSuccess: null,
 };
 
 
 export const reducer = createReducer(
   initialState,
-  on(sendingCustomerSupportMessage, (state, action) => (
-    {
+  on(
+    fromSupportActions.sendingCustomerSupportMessage,
+    (state, action) => ({
       ...state,
-      name: action.data.name
-    }
-  ))
+      name: action.data.name,
+    })
+  ),
+  on(
+    fromSupportActions.sendMessageStatus,
+    (state, action) => ({
+      ...state,
+      isSentSuccess: action.isSentSuccess,
+    })
+  )
 );
 
